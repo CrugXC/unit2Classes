7e7574953867573740ea8bde9b5d5a297e2254cd
CrugXC <jtrixie@NN123-HA-22.HS.sd203.org>
2015-09-10 15:10:03 -0500

    Started work

---
+++ Unit2Practice/Car.java
@@ -0,0 +1,44 @@
+
+
+/**
+ * The class Car has a certain fuel efficiency and a certain amount of fuel in the gas tank. A Car may be driven
+ * a specified distance which reduces the amount of gas in the tank.
+ * 
+ * @author Rixie 
+ * @version 9/10/15
+ */
+public class Car
+{
+    /** Specifies the fuel efficiency of the Car in units of miles per gallon (mpg)*/
+    private double fuelEfficiency;
+    
+    /** Specifies the amount of fuel in gas tank in gallons */
+    private double currentFuel;
+
+    /**
+     * Default constructor for objects of class Car
+     */
+    public Car()
+    {
+        // initialise instance variables
+        x = 0;
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     *    that describes the operation of the method
+     *
+     * @pre        preconditions for the method
+     *            (what the method assumes about the method's parameters and class's state)
+     * @post    postconditions for the method
+     *            (what the method guarantees upon completion)
+     * @param    y    description of parameter y
+     * @return    description of the return value
+     */
+    public int sampleMethod(int y)
+    {
+        // put your code here
+        return x+y;
+    }
+
+}
