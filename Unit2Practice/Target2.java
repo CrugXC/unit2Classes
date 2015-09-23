9a2ec37bd4fed73ee3e700d93403fe74878ae94c
CrugXC <jayrixie@gmail.com>
2015-09-23 16:07:56 -0500

    Update Target2.java

--- Unit2Practice/Target2.java
+++ Unit2Practice/Target2.java
@@ -1,7 +1,4 @@
-import java.awt.geom.Line2D;
-import java.awt.geom.Point2D;
 import java.awt.Color;
-import java.awt.Graphics;
 import java.util.Random;
 import java.awt.Graphics2D;
 import java.awt.geom.Ellipse2D;
@@ -14,29 +11,28 @@ public class Target2
     private int rings;
     private Color[] colorArray = {Color.RED, Color.BLUE, Color.MAGENTA, Color.ORANGE, Color.YELLOW, Color.CYAN, Color.GRAY, Color.GREEN, Color.PINK};
     private int color;
+    private int r;
     
     
-    public Target2(int x, int y, int numberOfRings)
+    public Target2(int x, int y, int numberOfRings, radius)
     {
         xCord = x;
         yCord = y;
         rings = numberOfRings;
-        
+        r = radius;
     }
     
     public void draw(Graphics2D g2)
     {
-        int offset = 0;
         
         Random r1 = new Random();
         
         for(int i = 0; i < rings; i ++)
         {
-            Ellipse2D.Double circle = new Ellipse2D.Double(xCord + (offset * 20), yCord + (offset * 20), (50 * offset), (50 * offset));;
+            Ellipse2D.Double circle = new Ellipse2D.Double(i * r, i * r, (10 - i) * r, (10-i) *r);
             color = r1.nextInt(8);
             g2.setPaint(colorArray[color]);
             g2.fill(circle);
-            offset += 1;
             
         }
     }
