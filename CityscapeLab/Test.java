6a6edcc11877045bf28a4bf92b16a96bdb12388c
CrugXC <jayrixie@gmail.com>
2015-10-10 21:54:05 -0500

    Revert "Revert "Test Work and Failed Work on Stars that was Discarded""
    This reverts commit 8b2036950eab36e2bcaef108df16487ece87c5d7.

--- CityscapeLab/Test.java
+++ CityscapeLab/Test.java
@@ -1,6 +1,14 @@
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import javax.swing.JComponent;
+import java.awt.Color;
+import java.util.Random;
+import java.awt.geom.Ellipse2D;
+import java.awt.geom.Rectangle2D;
+import java.awt.geom.Line2D;
+import java.awt.geom.Point2D;
+import java.awt.geom.QuadCurve2D;
+
 
 public class Test extends JComponent
 {
@@ -8,7 +16,23 @@ public class Test extends JComponent
     public void paintComponent(Graphics g)
     {
         Graphics2D g2 = (Graphics2D) g;
-        /* Stars testing = new Stars(10, 5, 5); */
-        /* testing.draw(g2); */
+        
+        
+        Rectangle2D.Double bridgeRoad = new Rectangle2D.Double(500, 500, 100, 20);
+        Rectangle2D.Double tower1 = new Rectangle2D.Double(525, 450, 7, 100);
+        Rectangle2D.Double tower2 = new Rectangle2D.Double(568, 450, 7, 100);
+        QuadCurve2D.Double rope1 = new QuadCurve2D.Double(500, 500, 525, 500, 525, 450);
+        QuadCurve2D.Double rope1Fill = new QuadCurve2D.Double(500, 498, 525, 498, 525, 448);
+        
+        
+        g2.setColor(Color.BLACK);
+        g2.fill(bridgeRoad);
+        g2.fill(tower1);
+        g2.fill(tower2);
+        g2.fill(rope1);
+        
+        
+        g2.setColor(Color.WHITE);
+        g2.fill(rope1Fill);
     }
 }
